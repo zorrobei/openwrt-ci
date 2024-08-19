a82ab7c24eaabe3be9e5a33490aa463fc9d36bdc
Zorro Bei <hzbeiyx@gmail.com>
2024-08-19 12:46:46 +0800

    Update diy-script.sh
    Change default IP and shell.

--- diy-script.sh
+++ diy-script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 修改默认IP
-# sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.2.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
-# 更改默认 Shell 为 zsh
-# sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+# 更改默认 Shell 为 bash
+sed -i 's/\/bin\/ash/\/usr\/bin\/bash/g' package/base-files/files/etc/passwd
 
 # TTYD 免登录
 # sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
